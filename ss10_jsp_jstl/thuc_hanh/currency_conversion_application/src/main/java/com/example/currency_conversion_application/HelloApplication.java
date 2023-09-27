a76e90a94590ae0b320e0f23ace26d588d2fbf80
Khoa6698 <lekhoa661998@gmail.com>
2023-09-27 15:25:31 +0700

    sua file

--- ss10_jsp_jstl/thuc_hanh/currency_conversion_application/src/main/java/com/example/currency_conversion_application/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.currency_conversion_application;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
